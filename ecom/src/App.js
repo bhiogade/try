2efd7207173e7f95fbea28cea7913358e13ca598
Sanket Bhiogade <sanket.bhiogade95@gmail.com>
2021-07-29 19:26:06 +0530

    t

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -1,17 +1,20 @@
 //import logo from './logo.svg';
 //import './App.css';
 import { Redirect, Route, Switch } from "react-router";
+import MenuAppBar from "./AppBar";
 import Home from "./Home";
 import ProductDetails from "./ProductDetails";
 
 function App() {
   return (
     <>
+      <MenuAppBar />
       <Switch>
       <Route path="/home" component={Home} />
       <Route path="/product-details/:id" component={ProductDetails} />
       <Redirect to="/home" />
-    </Switch>
+      </Switch>
+      
       ecomm
     </>
   );
--- ecom/src/Home.jsx
+++ ecom/src/Home.jsx
@@ -1,10 +1,11 @@
-import MenuAppBar from "./AppBar";
-import Filters from "./Filters";
-import Products from "./Products";
+//import MenuAppBar from "./AppBar";
+//import Filters from "./Filters";
+//import Products from "./Products";
+import AppBar from "./AppBar";
 
 const Home = () => {
     return (
-    "Home"
+        "Home"
      );
 }
  
